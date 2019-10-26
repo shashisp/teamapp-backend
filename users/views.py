076f3a6636be60b9e7ed1b00403dcc7f566da13e
Shashi Kumar P <shashiessp@gmail.com>
2019-10-26 09:19:49 -0500

    add django rest framework and update API endpoints

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from users.models import CustomUser
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
